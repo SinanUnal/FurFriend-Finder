43d676c7f0062fe41fb764df877ca8d5238f1669
SinanUnal <usinan9@gmail.com>
2024-02-18 21:39:56 -0800

    signup page form styles added

--- client-side/src/components/Auth/Login.jsx
+++ client-side/src/components/Auth/Login.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 export default function Login() {
@@ -29,17 +31,7 @@ export default function Login() {
           } 
           // For non-admin users, check their userType
           else {
-            // switch (data.user.userType) {
-            //   case 'adopter':
-            //     navigate('/adopter-dashboard');
-            //     break;
-            //   case 'giver':
-            //     navigate('/giver-dashboard');
-            //     break;
-            //   default:
-            //     navigate('/adopter-dashboard');
-            //     break;
-            // }
+        
             const user = data.user;
             let adopterId, giverId;
             if (user.userType === 'adopter') {
@@ -63,31 +55,44 @@ export default function Login() {
   }
 
   return (
-    <div>
-      <h1>This is login page</h1>
-      <input 
-        type="text"
-        className="input-field"
-        placeholder="Username"
-        onChange={(e) => {
-          setUsername(e.target.value);
-        }}/>
-      <br />
-      <input 
-        type="password"
-        className="input-field"
-        placeholder="Password" 
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}/>
-      <br />
-      <p className="error-message">{error}</p>
-      <button
-        className="button" 
-        onClick={() => {
-          login();
-        }}>Login</button>
-      <p>You do not have an account ? <Link to={'/'} className="link">Signup</Link></p>
+    <div className='container mt-5'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3'>
+          <h1>This is login page</h1>
+          <form>
+             <div className='col-12 mb-3'>
+              <input 
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                onChange={(e) => {
+                  setUsername(e.target.value);
+              }}/>
+             </div>
+             <div className='col-12 mb-3'>
+              <input 
+                type="password"
+                className="form-control"
+                placeholder="Password" 
+                onChange={(e) => {
+                  setPassword(e.target.value);
+              }}/>
+            </div>
+            <div className='mb-3'>
+              <p className="error-message">{error}</p>
+            </div>
+            <div className='mb-3'>
+              <button
+                className="btn btn-primary w-100" 
+                onClick={() => {
+                  login();
+              }}>Login</button>
+            </div>
+          </form>
+        </div>   
+      </div>
+      
+      <p className='text-center'>You do not have an account ? <Link to={'/'} className="link">Signup</Link></p>
     </div>
   )
 }
@@ -110,32 +115,3 @@ export default function Login() {
 
 
 
-//   return (
-//     <div>
-//       <h1>This is login page</h1>
-//       <input 
-//         type="text"
-//         className="input-field"
-//         placeholder="Username"
-//         onChange={(e) => {
-//         setUsername(e.target.value);
-//       }}/>
-//       <br />
-//       <input 
-//         type="password"
-//         className="input-field"
-//         placeholder="Password" 
-//         onChange={(e) => {
-//         setPassword(e.target.value);
-//       }}/>
-//       <br />
-//       <p className="error-message">{error}</p>
-//       <button
-//         className="button" 
-//         onClick={() => {
-//         login();
-//       }}>Login</button>
-//       <p>You do not have an account ? <Link to={'/'} className="link">Signup</Link></p>
-//     </div>
-//   )
-// }
